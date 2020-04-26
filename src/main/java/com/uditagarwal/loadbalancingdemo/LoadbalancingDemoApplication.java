22d91473c5686cccfc61358f76530097fdfc82da
Udit Agarwal <udiagarw@adobe.com>
2020-04-27 00:23:35 +0530

    First commit

---
+++ src/main/java/com/uditagarwal/loadbalancingdemo/LoadbalancingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.uditagarwal.loadbalancingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uditagarwal/loadbalancingdemo/LoadbalancingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uditagarwal.loadbalancingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
