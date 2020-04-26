e2e5a13233129c4994e8b9661c83270ddb540a30
Udit Agarwal <udiagarw@adobe.com>
2020-04-27 00:31:11 +0530

    Add hello world api

---
+++ src/main/java/com/uditagarwal/loadbalancingdemo/conntrollers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.uditagarwal.loadbalancingdemo.conntrollers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "Hello World!";
+  }
+}
