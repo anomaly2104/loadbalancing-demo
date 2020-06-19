d9ddbd38b6c8bfe6e18e54a3fdb3aff192cfe70f
Udit Agarwal <hello@uditagarwal.com>
2020-06-19 17:56:42 +0530

    Log calls to health check endpoint

--- src/main/java/com/uditagarwal/loadbalancingdemo/conntrollers/HealthCheckController.java
+++ src/main/java/com/uditagarwal/loadbalancingdemo/conntrollers/HealthCheckController.java
@@ -1,13 +1,17 @@
 package com.uditagarwal.loadbalancingdemo.conntrollers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+    Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
-  @RequestMapping("/health")
-  public String health() {
-    return "All good!";
-  }
+    @RequestMapping("/health")
+    public String health() {
+      logger.info("Health check recived");
+      return "All good!";
+    }
 }
--- src/main/java/com/uditagarwal/loadbalancingdemo/conntrollers/HelloWorldController.java
+++ src/main/java/com/uditagarwal/loadbalancingdemo/conntrollers/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.uditagarwal.loadbalancingdemo.conntrollers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+  Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
   @RequestMapping("/hello")
   public String hello() throws InterruptedException {
-    System.out.println("Hello api " + Math.random());
+    logger.info("Hello api called");
     Thread.sleep(50);
     return "Hello World!";
   }
